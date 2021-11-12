b1a9e4e50131eb58a542fc1ef1a02e20578a9460
erikagonzalez5 <erikagonzalez5@gmail.com>
2021-11-12 11:01:06 -0800

    upload

--- Vacationpy/api_keys.py
+++ Vacationpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7c7250c0839b1d343f11e121645501f9"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBN4dLaKJsJDl4jc5QHoXeHtoRVsW-te_k"
